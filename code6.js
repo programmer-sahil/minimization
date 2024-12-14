9179e517ec43e68152a4f0b2d6e052f389242c3e
SAHIL <117796859+programmer-sahil@users.noreply.github.com>
2024-12-14 12:43:43 +0530

    Update code6.js

--- code6.js
+++ code6.js
@@ -1,1 +1,13 @@
+let xhr = new XMLHttpRequest();
+xhr.open("GET", "data.xml", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    let xmlDoc = xhr.responseXML;
+    let elements = xmlDoc.getElementsByTagName("item");
+    for (let i = 0; i < elements.length; i++) {
+      console.log(elements[i].childNodes[0].nodeValue);
+    }
+  }
+};
+xhr.send();
 
